009e5277fbbad3d9752badcd56206178cc03510f
zkr024 <zkr.bm.02403@gmail.com>
2022-03-14 17:02:51 -0500

    add function to add items

--- app.js
+++ app.js
@@ -1,36 +1,112 @@
 const upForm = document.querySelector('#form');
-const bookTitle = document.getElementById('bookTitle');
-const author = document.getElementById('author');
 const submit = document.querySelector('.submit-btn');
+const bTitle = document.getElementById('bookTitle');
+const bAuthor = document.getElementById('bAuthor');
 
-let array = [];
+const data = {}
+
+upForm.addEventListener('change', (e) => {
+    data[e.target.name] = e.target.value;    
+    const string = JSON.stringify(data);
+    localStorage.setItem('info', string);
+})
+
+let newData = [];
+let reload = [];
 
 submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    array.push(bookTitle.value);
-    array.push(author.value);
-    console.log(array);
-});
+    if(bTitle.value == "" || bAuthor.value == ""){
+        return null;
+    } else {
+        if(localStorage.length !== 0) {
+            e.preventDefault();
+            const infoBook = JSON.parse(localStorage.getItem('info'));
+            newData.push(infoBook);
+            const newLocal = JSON.stringify(newData);
+            localStorage.setItem('recent', newLocal);
+            const reloadBook = JSON.parse(localStorage.getItem('recent'));
+        }
+        reload.push(reloadBook);
+        newData.forEach(value => {
+            if(newData[newData.length-1] == value){
+                author(value);  
+            }
+        }); 
+    }
+})
 
-upForm.insertAdjacentHTML("beforebegin" `
-<div class='bTitle'></div>
-<div class='bAuthor'></div>
-<button class='button'>Remove</button>
-`)
+function author (array){
+    const newFile = document.querySelector('.bookDetails');
+    const divOne = document.createElement('div');
+    divOne.textContent = array.book;
+    newFile.appendChild(divOne);
+    const divTwo = document.createElement('div');
+    divTwo.textContent = array.author;
+    newFile.appendChild(divTwo);
+    const gone = document.createElement('button');
+    gone.classList.add('gone');
+    gone.textContent = 'Remove';
+    newFile.appendChild(gone);
+    const separate = document.createElement('hr');
+    newFile.appendChild(separate);
+}
+
+window.addEventListener('load', () => {
+    reload.forEach(value => {
+        if(reload[reload.length-1] == value){
+            author(value);  
+        }
+    })
+})
 
 
 
 
 
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+
+    
+    
+//});
+
+// const string = JSON.stringify(data);
+// localStorage.setItem('info', string);
+// const infoBook = JSON.parse(localStorage.getItem('info'));
+
+// if (localStorage.length !== 0){
+    // const infoBook = JSON.parse(localStorage.getItem('info'));
+    // function newBook () {
+        // const divOne = document.createElement('div');
+        // divOne.textContent = infoBook.book;
+        // newFile.appendChild(divOne);
+    // }
+    
+// }
 
 
+// newBook();
+    
+    
+    
+    
+    
+// }
 
-
-
-
-
-
-
-
-
+// upForm.insertAdjacentHTML("beforebegin" `
+// <div class='bTitle'></div>
+// <div class='bAuthor'></div>
+// <button class='button'>Remove</button>
+// `)
+// 
 
