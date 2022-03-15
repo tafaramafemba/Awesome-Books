b1058b1ec9c6c25819e899c9c04adfdad1374672
zkr024 <zkr.bm.02403@gmail.com>
2022-03-15 16:11:16 -0500

    Correct Linter errors

--- app2.js
+++ app2.js
@@ -1,41 +1,36 @@
 const booksSection = document.querySelector('.books');
 
 class Awesome {
-    constructor (){
-        this.books = [];
-        this.newBooks = [];
+  constructor() {
+    this.books = [];
+    this.newBooks = [];
+  }
+  add(data){
+    let temp = [];
+    temp.push(data);
+    this.books = temp;
+  }
+  addBook(data){
+    let temp = [];
+    temp.push(data);
+    this.newBooks = temp;
+  }
+  delete(number){
+    this.books.splice(number,1);
+    window.localStorage.setItem('bookArray', JSON.stringify(this.books));
+    for (let k = 0; k < this.books.length; k += 1) {
+      window.localStorage.setItem(k.toString(), JSON.stringify(this.books[k]));
     }
-
-    add(data){
-        let temp = []
-        temp.push(data);
-        this.books = temp;
-    }
-
-    addBook(data){
-        let temp = []
-        temp.push(data);
-        this.newBooks = temp;
-    }
-
-    delete(number){
-        this.books.splice(number,1);
-        window.localStorage.setItem('bookArray', JSON.stringify(this.books));
-        for (let k = 0; k < this.books.length; k += 1) {
-            window.localStorage.setItem(k.toString(), JSON.stringify(this.books[k]));
-        }
-        window.location.reload();    
-    }
-
-    displayObject(number) {
-        return `  
-            <div class="title">${this.books[number].title}</div>
-            <div class="author">${this.books[number].author}</div>
-            <button type="button" class="remove">Remove</button>
-            <hr>
-        `;
-    }
-
+    window.location.reload();    
+  }
+  displayObject(number) {
+    return `  
+      <div class="title">${this.books[number].title}</div>
+      <div class="author">${this.books[number].author}</div>
+      <button type="button" class="remove">Remove</button>
+      <hr>
+    `;
+  }
 }
 
 const awesome = new Awesome();
@@ -43,47 +38,47 @@ const awesome = new Awesome();
 awesome.add({title: 'Chronicles of Narnia', author: 'C.S Lewis'});
 
 if (window.localStorage.getItem('bookArray') !== null) {
-    const array = JSON.parse(window.localStorage.getItem('bookArray'));
-    for (let m = 0; m < array.length; m += 1) {
-        awesome.newBooks[m] = JSON.parse(window.localStorage.getItem(m.toString()));
-    }
-    awesome.books = awesome.newBooks;
+  const array = JSON.parse(window.localStorage.getItem('bookArray'));
+  for (let m = 0; m < array.length; m += 1) {
+    awesome.newBooks[m] = JSON.parse(window.localStorage.getItem(m.toString()));
+  }
+  awesome.books = awesome.newBooks;
 }
 
 for (let i = 0; i < awesome.books.length; i += 1) {
-    const newElement = document.createElement('section');
-    newElement.innerHTML = awesome.displayObject(i);
-    booksSection.appendChild(newElement);
+  const newElement = document.createElement('section');
+  newElement.innerHTML = awesome.displayObject(i);
+  booksSection.appendChild(newElement);
 }
 
 for (let j = 0; j < awesome.books.length; j += 1) {
-    const removeButton = document.querySelectorAll('.remove');
-    removeButton[j].addEventListener('click', () => { awesome.delete(j); });
+  const removeButton = document.querySelectorAll('.remove');
+  removeButton[j].addEventListener('click', () => { awesome.delete(j); });
 }
 
 const newTitle = document.getElementById('new-title');
 const newAuthor = document.getElementById('new-author');
 const add = document.getElementById('add');
 const newBook = {
-    title: '',
-    author: '',
+  title: '',
+  author: '',
 };
 
 add.addEventListener('click', () => {
-    newBook.title = newTitle.value;
-    newBook.author = newAuthor.value;
+  newBook.title = newTitle.value;
+  newBook.author = newAuthor.value;
 
-    awesome.books[awesome.books.length] = newBook;
+  awesome.books[awesome.books.length] = newBook;
 
-    window.localStorage.setItem('bookArray', JSON.stringify(awesome.books));
-    for (let k = 0; k < awesome.books.length; k += 1) {
-        window.localStorage.setItem(k.toString(), JSON.stringify(awesome.books[k]));
-    }
+  window.localStorage.setItem('bookArray', JSON.stringify(awesome.books));
+  for (let k = 0; k < awesome.books.length; k += 1) {
+    window.localStorage.setItem(k.toString(), JSON.stringify(awesome.books[k]));
+  }
 });
 
 window.localStorage.clear();
 
 window.localStorage.setItem('bookArray', JSON.stringify(awesome.books));
 for (let k = 0; k < awesome.books.length; k += 1) {
-    window.localStorage.setItem(k.toString(), JSON.stringify(awesome.books[k]));
+  window.localStorage.setItem(k.toString(), JSON.stringify(awesome.books[k]));
 }
